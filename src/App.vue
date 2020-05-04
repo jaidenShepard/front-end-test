638ebf09e98a847661c91f9df9fc5ca43f13d504
Jay <jay.shepard@protonmail.com>
2020-05-04 10:25:26 -0400

    Initial test starting files

---
+++ src/App.vue
@@ -0,0 +1,22 @@
+<template>
+    <div class="app">
+        <router-link to="/">Home</router-link>
+
+        <router-view/>
+    </div>
+</template>
+
+<script>
+export default {
+    name: 'App',
+};
+</script>
+
+<style lang="scss">
+body {
+    color: #2c3e50;
+    text-align: center;
+    -webkit-font-smoothing: antialiased;
+    -moz-osx-font-smoothing: grayscale;
+}
+</style>
