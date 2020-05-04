b15bec2cbce8137089a8c0b93bd8d3443725a45b
Jay <jay.shepard@protonmail.com>
2020-05-04 10:56:19 -0400

    Code formatting cleanup

--- src/App.vue
+++ src/App.vue
@@ -1,22 +1,22 @@
 <template>
-    <div class="app">
-        <router-link to="/">Home</router-link>
+  <div class="app">
+    <router-link to="/">Home</router-link>
 
-        <router-view/>
-    </div>
+    <router-view />
+  </div>
 </template>
 
 <script>
 export default {
-    name: 'App',
+  name: "App"
 };
 </script>
 
 <style lang="scss">
 body {
-    color: #2c3e50;
-    text-align: center;
-    -webkit-font-smoothing: antialiased;
-    -moz-osx-font-smoothing: grayscale;
+  color: #2c3e50;
+  text-align: center;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
 }
 </style>
